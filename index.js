15b448e1ea34c96a938d673ef778d5845c49df7c
thtTAMALtht <thtamal.cse@gmail.com>
2025-11-09 23:38:34 +0600

    listing get api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 //middleWare
@@ -27,12 +27,14 @@ async function run() {
     await client.connect();
     const myDB = client.db("pawMartDB");
     const userCollection = myDB.collection("users");
+    const listingCollection = myDB.collection("listings");
 
     //users realted API...................
     app.post("/users", async (req, res) => {
       const userDetails = req.body;
       const email = req.body.email;
-      const existingUser = { email: email };
+      const query = { email: email };
+      const existingUser = await userCollection.findOne(query);
       if (existingUser) {
         console.log({ message: "this user already in database" });
       } else {
@@ -41,7 +43,28 @@ async function run() {
       }
     });
 
-    //PRODUCT realted API...................
+    //Listing realted API...................
+
+    app.post("/listings", async(req, res) => {
+      const listingDetails = req.body;
+      const result = await listingCollection.insertOne(listingDetails);
+      res.send(result)
+    });
+
+    app.get("/listings", async(req, res) => {
+        const cursor = listingCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    });
+
+    app.get("/listings/:id", async(req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await listingCollection.findOne(query);
+        res.send(result);
+       
+    });
+
     //OTHERS realted API...................
     //EXTRA realted API...................
 
