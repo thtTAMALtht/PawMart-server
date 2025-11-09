606abd3900eb428439db654498b552f70b70bdf9
thtTAMALtht <thtamal.cse@gmail.com>
2025-11-09 20:22:32 +0600

    users added in database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
+require("dotenv").config();
 
 //middleWare
 app.use(cors());
@@ -25,10 +25,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const myDB = client.db("pawMartDB");
+    const userCollection = myDB.collection("users");
 
+    //users realted API...................
+    app.post("/users", async (req, res) => {
+      const userDetails = req.body;
+      const email = req.body.email;
+      const existingUser = { email: email };
+      if (existingUser) {
+        console.log({ message: "this user already in database" });
+      } else {
+        const result = await userCollection.insertOne(userDetails);
+        res.send(result);
+      }
+    });
 
-
-
+    //PRODUCT realted API...................
+    //OTHERS realted API...................
+    //EXTRA realted API...................
 
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
