13712fd84eaea547a2e34888e4651d614c47ffe7
thtTAMALtht <thtamal.cse@gmail.com>
2025-11-09 16:38:10 +0600

    server running

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+//middleWare
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
