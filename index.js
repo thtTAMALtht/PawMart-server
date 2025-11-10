732be0967f583aa0c399594f528f1a4c170d52c5
thtTAMALtht <thtamal.cse@gmail.com>
2025-11-10 18:40:54 +0600

    listing delete api added

--- index.js
+++ index.js
@@ -71,6 +71,15 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/listings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await listingCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    
+
     //Orders realted API...................
     app.post("/orders", async (req, res) => {
       const orderDetails = req.body;
